19161daee0071d7ce92237feace3b9248a4659fb
Aisha Ramiro <aishamontealto@gmail.com>
2022-05-31 22:11:42 -0300

    add services form

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,3 +1,5 @@
+import {parse, v4 as uuidv4} from 'uuid'
+
 import styles from './Project.module.css'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
@@ -7,6 +9,7 @@ import Container from '../layout/Container'
 import Message from '../layout/Message'
 
 import ProjectForm from '../project/ProjectForm'
+import ServiceForm from '../service/ServiceForm'
 
 function Project() {
 
@@ -14,6 +17,7 @@ function Project() {
 
   const [project, setProject] = useState([])
   const [showProjectForm, setShowProjectForm] = useState(false)
+  const [showServiceForm, setShowServiceForm] = useState(false)
   const [message, setMessage] = useState()
   const [type, setType] = useState()
 
@@ -34,7 +38,54 @@ function Project() {
     setShowProjectForm(!showProjectForm)
   }
 
+  function toggleServiceForm() {
+    setShowServiceForm(!showServiceForm)
+  }
+
+  function createService(project){
+    setMessage('')
+
+    //last service
+    const lastService = project.services[project.services.length - 1]
+
+    lastService.id = uuidv4()
+
+    const lastServiceCost = lastService.cost
+    const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
+
+    //maximum value validation
+    if(newCost > parseFloat(project.budget)) {
+      setMessage('Orçamento ultrapassado, verifique o valor do serviço')
+      setType('error')
+      project.services.pop()
+      return false
+    }
+
+    //add service cost to project total cost
+    project.cost = newCost
+
+    //update project
+
+    fetch(`http://localhost:5000/projects/${project.id}`,{
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(project)
+    }).then(
+      (resp) => resp.json())
+      .then((data) => {
+        //exibir serviços
+
+      })
+    
+    .catch(err => console.log(err))
+
+  }
+
   function editPost(project) {
+    setMessage('')
+
     if (project.budget < project.cost) {
       setMessage('O orçamento não pode ser menor que o custo do projeto!')
       setType('error')
@@ -94,6 +145,25 @@ function Project() {
               )}
 
             </div>
+            <div className={styles.serviceFormContainer}>
+              <h2>Adicione um serviço: </h2>
+              <button className={styles.btn} onClick={toggleServiceForm}>
+                {!showServiceForm ? 'Adicionar Serviço' : 'Fechar'}
+              </button>
+              <div className={styles.projectInfo}>
+                {showServiceForm && (
+                  <ServiceForm
+                    handleSubmit={createService}
+                    btnText='Adicionar serviço'
+                    projectData={project}                  />
+                )}
+              </div>
+
+            </div>
+              <h2>Serviços</h2>
+              <Container customClass='start'>
+                <p>Serviços</p>
+              </Container>
           </Container>
         </div>
 
