24feee49736b32ede82b60756385b6001146046c
Aisha Ramiro <aishamontealto@gmail.com>
2022-06-02 14:02:32 -0300

    remove project

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -10,29 +10,122 @@ import Message from '../layout/Message'
 
 import ProjectForm from '../project/ProjectForm'
 import ServiceForm from '../service/ServiceForm'
+import ServiceCard from '../service/ServiceCard'
+
 
 function Project() {
-
-  const { id } = useParams()
-
+  let { id } = useParams()
   const [project, setProject] = useState([])
   const [showProjectForm, setShowProjectForm] = useState(false)
   const [showServiceForm, setShowServiceForm] = useState(false)
-  const [message, setMessage] = useState()
-  const [type, setType] = useState()
+  const [services, setServices] = useState([])
+  const [message, setMessage] = useState('')
+  const [type, setType] = useState('success')
 
   useEffect(() => {
-    fetch(`http://localhost:5000/projects/${id}`, {
-      method: 'GET',
+    // Para ver o loading
+    setTimeout(
+      () =>
+        fetch(`http://localhost:5000/projects/${id}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((resp) => resp.json())
+          .then((data) => {
+            setProject(data)
+            setServices(data.services)
+          }),
+      0,
+    )
+  }, [id])
+
+  function editPost(project) {
+    // budget validation
+    if (project.budget < project.cost) {
+      setMessage('O Orçamento não pode ser menor que o custo do projeto!')
+      setType('error')
+      return false
+    }
+
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
       },
-    }).then(resp => resp.json())
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
       .then((data) => {
         setProject(data)
+        setShowProjectForm(!showProjectForm)
+        setMessage('Projeto atualizado!')
+        setType('success')
       })
-      .catch(err => console.log(err))
-  }, [id])
+  }
+
+  function createService(project) {
+    // last service
+    const lastService = project.services[project.services.length - 1]
+
+    lastService.id = uuidv4()
+
+    const lastServiceCost = lastService.cost
+
+    const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
+
+    // maximum value validation
+    if (newCost > parseFloat(project.budget)) {
+      setMessage('Orçamento ultrapassado, verifique o valor do serviço!')
+      setType('error')
+      project.services.pop()
+      return false
+    }
+
+    // add service cost to project cost total
+    project.cost = newCost
+
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setServices(data.services)
+        setShowServiceForm(!showServiceForm)
+        setMessage('Serviço adicionado!')
+        setType('success')
+      })
+  }
+
+  function removeService(id, cost) {
+    const servicesUpdated = project.services.filter(
+      (service) => service.id !== id,
+    )
+
+    const projectUpdated = project
+
+    projectUpdated.services = servicesUpdated
+    projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost)
+
+    fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(projectUpdated),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProject(projectUpdated)
+        setServices(servicesUpdated)
+        setMessage('Serviço removido com sucesso!')
+      })
+  }
 
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm)
@@ -40,74 +133,6 @@ function Project() {
 
   function toggleServiceForm() {
     setShowServiceForm(!showServiceForm)
-  }
-
-  function createService(project){
-    setMessage('')
-
-    //last service
-    const lastService = project.services[project.services.length - 1]
-
-    lastService.id = uuidv4()
-
-    const lastServiceCost = lastService.cost
-    const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
-
-    //maximum value validation
-    if(newCost > parseFloat(project.budget)) {
-      setMessage('Orçamento ultrapassado, verifique o valor do serviço')
-      setType('error')
-      project.services.pop()
-      return false
-    }
-
-    //add service cost to project total cost
-    project.cost = newCost
-
-    //update project
-
-    fetch(`http://localhost:5000/projects/${project.id}`,{
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(project)
-    }).then(
-      (resp) => resp.json())
-      .then((data) => {
-        //exibir serviços
-
-      })
-    
-    .catch(err => console.log(err))
-
-  }
-
-  function editPost(project) {
-    setMessage('')
-
-    if (project.budget < project.cost) {
-      setMessage('O orçamento não pode ser menor que o custo do projeto!')
-      setType('error')
-      return false
-    }
-
-    fetch(`http://localhost:5000/projects/${id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(project),
-    })
-      .then(resp => resp.json())
-      .then((data) => {
-        setProject(data)
-        setShowProjectForm(false)
-        setMessage('Projeto atualizado!')
-        setType('sucess')
-      })
-      .catch(err => console.log(err))
-
   }
 
   return (
@@ -161,10 +186,21 @@ function Project() {
 
             </div>
               <h2>Serviços</h2>
-              <Container customClass='start'>
-                <p>Serviços</p>
-              </Container>
-          </Container>
+              <Container customClass="start">
+              {services.length > 0 &&
+                services.map((service) => (
+                  <ServiceCard
+                    id={service.id}
+                    name={service.name}
+                    cost={service.cost}
+                    description={service.description}
+                    key={service.id}
+                    handleRemove={removeService}
+                  />
+                ))}
+              {services.length === 0 && <p>Não há serviços cadastrados.</p>}
+            </Container>
+            </Container>
         </div>
 
       ) : (
